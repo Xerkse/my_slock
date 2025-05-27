1c43cd8fee4a24ed43b0bc48d53923288f77790d
Xerkse <cjmp9876@gmail.com>
2025-05-27 09:46:16 -0500

    tokyonight colors

--- config.def.h
+++ config.def.h
@@ -2,10 +2,11 @@
 static const char *user  = "xerkse";
 static const char *group = "xerkse";
 
+// tokyonight nvim colors
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#9ece6a",     /* after initialization */
+	[INPUT] =  "#7aa2f7",   /* during input */
+	[FAILED] = "#f7768e",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
